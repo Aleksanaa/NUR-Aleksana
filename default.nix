699a61262d4d07977896716ad23695a2e7730b01
aleksana <me@aleksana.moe>
2023-01-20 20:23:29 +0800

    initial commit

--- default.nix
+++ default.nix
@@ -14,7 +14,11 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
+  # example-package = pkgs.callPackage ./pkgs/example-package { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
+  
+  go-musicfox = pkgs.callPackage ./pkgs/go-musicfox { };
+  gtkcord4 = pkgs.callPackage ./pkgs/gtkcord4 { };
+  payload-dumper-go = pkgs.callPackage ./pkgs/payload-dumper-go { };
 }
